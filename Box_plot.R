ac822e5f8562d56629fc3610ac04c8a21a531062
Laura Elliott, MPH <51793714+laurajel@users.noreply.github.com>
2019-08-03 16:21:41 -0400

    Update Box_plot.R

--- Box_plot.R
+++ Box_plot.R
@@ -4,8 +4,10 @@ library(gapminder)
 
 
 
-p=plot_ly(noaa_df, x = ~water_temp, y = ~cellcount, type="scatter", 
-          text = paste("Temperature: ", noaa_df$water_temp, "Cell count: ", noaa_df$cellcount, "Date: ", noaa_df$sample_date, sep = " "),
+p = noaa_data %>%
+    plot_ly(x = ~cellcount, y = ~water_temp, type="scatter", 
+          text = paste("Temperature: ", noaa_data$water_temp, "Cell count: ", noaa_data$cellcount, "Date: ", noaa_data$sample_date, sep = " "),
           mode = "markers", color = ~water_temp, size = ~cellcount)
+
 p
 
