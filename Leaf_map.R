c5c6b14ee244ff2ee39f01bdf068c5be26344c94
Laura Elliott, MPH <51793714+laurajel@users.noreply.github.com>
2019-08-03 16:22:26 -0400

    Update Leaf_map.R

--- Leaf_map.R
+++ Leaf_map.R
@@ -4,21 +4,19 @@ library(leaflet)
 library(tidyverse)
 library(RColorBrewer)
 
+noaa_data = readr::read_csv("NOAA_Shiny_ALL.csv")
+
 ############### Vilsualize cell count
-head(noaa_df)
-summary(noaa_df$cellcount)
 
-data = head(noaa_df, 100000)
-data = na.omit(data)
-summary(data$cellcount)
+summary(noaa_data$cellcount)
 mybins=seq(1000, 200000, by=30000)  ###### 
-mypalette = colorBin( palette="RdPu", domain=data$cellcount, na.color="transparent", bins=mybins)
+mypalette = colorBin( palette="RdPu", domain=noaa_data$cellcount, na.color="transparent", bins=mybins)
 
-mytext=paste("Cellcount: ", data$cellcount, "<br/>", "Stations: ", data$description, "<br/>", "Year: ", data$sample_date, sep="") %>%
+mytext=paste("Cellcount: ", noaa_data$cellcount, "<br/>", "Stations: ", noaa_data$description, "<br/>", "Water Temperature: ", noaa_data$water_temp, "<br/>", "Date: ", noaa_data$sample_date, sep="") %>%
   lapply(htmltools::HTML)
 
 
-leaflet(data) %>% 
+leaflet(noaa_data) %>% 
   addTiles()  %>% 
   setView( lat= 26, lng= -82 , zoom=4) %>%
   addProviderTiles("Esri.WorldImagery") %>%
@@ -28,5 +26,9 @@ leaflet(data) %>%
                    labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
   ) %>%
   addLegend( pal=mypalette, values=~cellcount, opacity=0.9, title = "Algal Cell count", position = "bottomright" )
+
+
+
+
 
 
