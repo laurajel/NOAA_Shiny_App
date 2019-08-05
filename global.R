e53004eda48a14dc3859033c1ee4146d7f5ddec6
Laura Elliott, MPH <51793714+laurajel@users.noreply.github.com>
2019-08-04 23:21:44 -0400

    Update global.R

--- global.R
+++ global.R
@@ -1,4 +1,12 @@
-noaa_df = read.csv(file = "NOAA_Shiny_ALL.csv")
+noaa_data = read.csv(file = "NOAA_Shiny_ALL.csv")
+
+library(shiny)
+library(ggplot2)
+library(maps)
+library(leaflet)
+library(tidyverse)
+library(RColorBrewer)
+library(dplyr)
 
 
 
